b04c0aa68d4e80609d781cf3059e59df096767c5
Leyva <oleyva@cps.k12.il.us>
2024-09-04 14:59:41 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,15 @@
+# ask someone what day it is
+
+# ask them for their favorite holiday
+
+# print out the day and their favorite holiday in a sentence
+
+# making a change to the file
+
+day_today = input("What day is it ?")
+print("The day is " + day_today)
+
+favorite_holiday = input("What is your favorite holiday?")
+print("My favorite holiday is " + favorite_holiday )
+
+print ("The day is" , day_today , "and my favorite holiday is " + favorite_holiday)
